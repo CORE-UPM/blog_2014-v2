1beced9926cc939e3915558fbfce0289ae4167e3
Santiago Pavon <santiago@dit.upm.es>
2014-03-31 21:45:39 +0200

    Fichero olvidado de congelar.

--- models/index.js
+++ models/index.js
@@ -3,17 +3,27 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
-// Configurar Sequelize para usar SQLite.
-// No hay Bases de datos, ni usuarios, ni passwords.
-// El fichero con la BBDD es blog.sqlite.
+// Configurar Sequelize para usar SQLite. Uso una expresion regular para extraer 
+// los valores de acceso a la base de datos
+var vals = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 
-var sequelize = new Sequelize(null,null,null,
-                              {dialect:"sqlite",
-                               storage:"blog.sqlite",
-                               define: {
-                                 underscored: false
-                               }
-                              });
+var DATABASE_PROTOCOL = vals[1];
+var DATABASE_DIALECT = vals[1];
+var DATABASE_USER = vals[2];
+var DATABASE_PASSWORD = vals[3];
+var DATABASE_HOST = vals[4];
+var DATABASE_PORT = vals[5];
+var DATABASE_NAME = vals[6];
+
+var sequelize = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, 
+            { dialect:  DATABASE_DIALECT, 
+              protocol: DATABASE_PROTOCOL, 
+              port:     DATABASE_PORT,
+              host:     DATABASE_HOST,
+              storage:  process.env.DATABASE_STORAGE,   // solo local en .env
+              omitNull: true,                           // para postgres
+              define: {underscored: false}
+            });
 
 
 // Importar la definicion de las clases.
